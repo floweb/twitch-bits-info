a4873268254334364525ad07bcac1921c7341e26
Florian Le Frioux <floweb@users.noreply.github.com>
2016-11-13 12:50:11 +0100

    Update cm_bits.py

--- cm_bits.py
+++ cm_bits.py
@@ -1,12 +1,22 @@
-import os
+from __future__ import print_function
+
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
 
 from twitchbitsinfo import TwitchBitsInfo
 
-os.environ["PYTWITCHER_CLIENT_ID"] = 'bc4ozy62dshy18hq1wp8nrhfz44rknd'
-
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
     try:
-        bits = TwitchBitsInfo()
+        config.readfp(open('config.ini'))
+        config_dict = {s: dict(config.items(s)) for s in config.sections()}
+    except FileNotFoundError:
+        print('WARNING: config.ini file does not exist, using default config values !')
+        config_dict = {'config': {}}
+
+    try:
+        bits = TwitchBitsInfo(**config_dict['config'])
     except KeyboardInterrupt:
         bits.close()
-        bits.log.info("Terminating...")
